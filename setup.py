62a256aca8b7aae09b0be63de945404b0238d62b
Quentin Lhoest <lhoest.q@gmail.com>
2022-06-16 17:49:12 +0200

    test hfh 0.8.0.rc1

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ REQUIRED_PKGS = [
     # for data streaming via http
     "aiohttp",
     # To get datasets from the Datasets Hub on huggingface.co
-    "huggingface-hub>=0.1.0,<1.0.0",
+    "huggingface-hub==0.8.0.rc1",
     # Utilities from PyPA to e.g., compare versions
     "packaging",
     "responses<0.19",
